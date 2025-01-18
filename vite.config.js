a0e61b18a26eab3ac5d9a886842dff62c8548c96
minjae <minjagot@gmail.com>
2025-01-18 23:32:38 +0900

    config base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-movie/',
   resolve: {
     alias: {
       "@component": path.resolve(__dirname, "src/component"),
