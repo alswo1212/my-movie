b07eebb73aeb4378c7ff33997c86f4cdb7a85690
minjae <minjagot@gmail.com>
2025-01-29 17:16:25 +0900

    add movie detail page

--- src/apis/movie.js
+++ src/apis/movie.js
@@ -35,16 +35,18 @@ const setPoster = async (movies) => {
   const response = await Promise.all(
     movies.map(({ movie_nm, open_dt }) => {
       const url = `${POSTER}?collection=kmdb_new2&ServiceKey=${POSTER_KEY}&title=${movie_nm}&releaseDts=${open_dt.replaceAll('-', '')}`
-      return axiosPoster.get(url)
+      return axiosPoster.get(url);
     }));
   response.forEach((rep, i) => {
     const datas = rep.data.Data;
     if (datas && datas.length) {
       const results = datas[0].Result;
       if (results && results.length) {
-        const posterUrl = results[0].posters.split('|')[0]
-        movies[i].poster_url = posterUrl
-        movies[i].director = results[0].directors.director[0].directorNm
+        const posterUrl = results[0].posters.split('|')[0];
+        movies[i].poster_url = posterUrl;
+        movies[i].director = results[0].directors.director[0].directorNm;
+        movies[i].movie_id = results[0].movieId;
+        movies[i].movie_seq = results[0].movieSeq;
       }
     }
   });
@@ -69,4 +71,27 @@ export const getSearchMovies = async (searchText, param = 'title', page = 0) => 
   }));
 
   return result;
+}
+
+export const getMovieByIdAndSeq = async (movieId, movieSeq) => {
+  const url = `${SEARCHMOVIELIST}?collection=kmdb_new2&detail=Y&ServiceKey=${POSTER_KEY}&use=극장용&movieId=${movieId}&movieSeq=${movieSeq}`;
+  const { data: { Data } } = await axiosPoster.get(url);
+  const Result = Data[0].Result;
+  if (!Result || !Result.length) {
+    return null;
+  }
+  const {
+    CommCodes, titleEtc, nation, genre, posters,
+    directors, actors, runtime, plots,
+  } = Result[0];
+
+  return {
+    nation, genre, runtime,
+    movie_cd: CommCodes.CommCode[0].CodeNo,
+    movie_nm: titleEtc.split('^')[0],
+    poster_url: posters.split('|')[0],
+    directors: directors.director,
+    actors: actors.actor,
+    plot: plots.plot[0].plotText
+  };
 }
--- src/page/MovieDetail.jsx
+++ src/page/MovieDetail.jsx
@@ -1,14 +1,95 @@
-﻿import { useEffect } from "react";
+﻿import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { getMovieByIdAndSeq } from "@apis/movie";
+import { Typography } from "@mui/material";
+import styled from "styled-components";
+
+const MovieInfo = styled.div`
+  ${props => props.infoname && `
+    &::before{
+      content: '${props.infoname}';
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 600;
+      line-height: 1.75;
+      min-width: 64px;
+      border-radius: 4px;
+      border: 1px solid rgba(25, 118, 210, 0.5);
+      color: #1976d2;
+      padding-inline: 8px;
+      margin-right: 10px;
+    }
+  `}
+  &>p{
+    font-family: 'RIDIBatang', sans-serif;
+    padding-left: 10px;
+  }
+  &>span{
+    display: block;
+    margin: 10px 0;
+    padding-left: 10px;
+  }
+`
 
 const MovieDetail = () => {
-    const params = useParams();
-    useEffect(() => {
+  const {movieId, movieSeq} = useParams();
+  const [movie, setMovie] = useState(null);
 
-    }, [])
-    return (
-        <div>Movie Detail</div>
-    )
+  useEffect(() => {
+    const setData = async () => {
+      const data = await getMovieByIdAndSeq(movieId, movieSeq);
+      if(data)
+        setMovie(data);
+    }
+    setData()
+  }, [])
+  return (
+    <div>
+      <br/>
+      {movie &&
+      <>
+      <div style={{display:'flex', gap:30}}>
+        <div style={{
+          flexBasis:'50%',
+          textAlign:'center'
+        }}>
+          <div>
+            <img src={movie.poster_url} alt={movie.movie_nm} style={{
+              width:'100%',
+              borderRadius:20,
+              boxShadow: '0 0 10px 0 #666',
+            }}/>
+          </div>
+        </div>
+        <div style={{
+          display:'flex', 
+          flexDirection:'column', 
+          gap:10,
+          flexBasis:'50%',
+        }}>
+          <Typography variant="h4" gutterBottom fontFamily={'GmarketSansBold'}>{movie.movie_nm}</Typography>
+          <MovieInfo infoname={'감독'}>
+            <span>{movie.directors.map(d => d.directorNm).join(', ')}</span>
+          </MovieInfo>
+          <MovieInfo infoname={'출연진'}>
+            <span>{movie.actors.filter((_, i) => i < 10).map(d => d.actorNm).join(', ')}</span>
+          </MovieInfo>
+          <MovieInfo infoname={'장르'}>
+            <span>{movie.genre}</span>
+          </MovieInfo>
+          <MovieInfo infoname={'줄거리'}>
+            <p style={{lineHeight: '1.5em'}}>{movie.plot}</p>
+          </MovieInfo>
+        </div>
+      </div>
+      <div>
+        리뷰자리
+      </div>
+      </>
+      }
+    </div>
+  )
 }
 
 export default MovieDetail
